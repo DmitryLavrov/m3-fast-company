65caf767e81b6ca91d5469983327dfe9b3d7a74f
DmitryLavrov <d3236562@mail.ru>
2022-02-08 15:37:35 +0300

    Models, routes and start mock data implemented

---
+++ server/routes/user.routes.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+const router = express.Router({mergeParams: true})
+
+router.patch('/:id', async (req, res) => {
+})
+
+router.get('/', async (req, res) => {
+})
+
+module.exports = router
