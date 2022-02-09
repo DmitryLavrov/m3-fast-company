21a1660625cf78efde5f8e1081ac072ac8235c66
DmitryLavrov <d3236562@mail.ru>
2022-02-09 12:31:37 +0300

    Route auth (signUp, signInWithPassword, token) implemented

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,14 +1,143 @@
 const express = require('express')
+const bcrypt = require('bcryptjs')
+const {check, validationResult} = require('express-validator')
+
+const {generateUserData} = require('../utils/helper')
+const User = require('../models/User')
+const tokenService = require('../services/token.service')
 
 const router = express.Router({mergeParams: true})
 
-router.post('/signUp', async (req, res) => {
-})
+router.post('/signUp', [
+  check('email', 'INVALID_EMAIL').isEmail(),
+  check('password', 'PASSWORD_MIN_LENGTH_8').isLength({min: 8}),
+  async (req, res) => {
+    try {
+      const errors = validationResult(req)
+      if (!errors.isEmpty()) {
+        return res.status(400).json({
+          error: {
+            message: errors.array()[0].msg,
+            code: 400
+          }
+        })
+      }
 
-router.post('/signInWithPassword', async (req, res) => {
-})
+      const {email, password} = req.body
+
+      const existingUser = await User.findOne({email})
+      if (existingUser) {
+        return res.status(400).json({
+          error: {
+            message: 'EMAIL_EXISTS',
+            code: 400
+          }
+        })
+      }
+
+      const hashedPassword = await bcrypt.hash(password, 12)
+
+      const newUser = await User.create({
+        ...generateUserData(),
+        ...req.body,
+        password: hashedPassword
+      })
+
+      const tokens = tokenService.generate({_id: newUser._id})
+      await tokenService.save(newUser._id, tokens.refreshToken)
+
+      res.status(201).send({...tokens, userId: newUser._id})
+
+    } catch (err) {
+      res.status(500).json({
+        message: 'Error on server. Try again later'
+      })
+    }
+  }
+])
+
+router.post('/signInWithPassword', [
+  check('email','INVALID_EMAIL').normalizeEmail().isEmail(),
+  check('password', 'EMPTY_PASSWORD').exists(),
+
+  async (req, res) => {
+    try {
+      const errors = validationResult(req)
+      if (!errors.isEmpty()) {
+        return res.status(400).json({
+          error: {
+            message: errors.array()[0].msg,
+            code: 400
+          }
+        })
+      }
+
+      const {email, password} = req.body
+
+      const existingUser = await User.findOne({email})
+      if (!existingUser) {
+        return res.status(400).json({
+          error: {
+            message: 'EMAIL_NOT_FOUND',
+            code: 400
+          }
+        })
+      }
+
+      const isPasswordsMatch = await bcrypt.compare(password, existingUser.password)
+
+      if (!isPasswordsMatch) {
+        return res.status(400).json({
+          error: {
+            message: 'INVALID_PASSWORD',
+            code: 400
+          }
+        })
+      }
+
+      const tokens = tokenService.generate({_id: existingUser._id})
+      await tokenService.save(existingUser._id, tokens.refreshToken)
+
+      res.status(200).send({...tokens, userId: existingUser._id})
+
+    } catch (err) {
+      res.status(500).json({
+        message: 'Error on server. Try again later'
+      })
+    }
+  }
+])
+
+function isTokenInvalid(data, dbToken) {
+  return !data || !dbToken || data._id !== dbToken.user?.toString()
+}
 
 router.post('/token', async (req, res) => {
+  try {
+    const {refresh_token: refreshToken} = req.body
+    const data = tokenService.validateRefresh(refreshToken)
+    const dbToken = await tokenService.findToken(refreshToken)
+
+    if (isTokenInvalid(data, dbToken)) {
+      return res.status(401).json({
+        error: {
+          message: 'UNAUTHORIZED',
+          code: 401
+        }
+      })
+    }
+
+    const tokens = tokenService.generate({_id: data._id})
+
+    await tokenService.save(data._id, tokens.refreshToken)
+
+    res.status(200).send({...tokens, userId: data._id})
+
+  } catch (err) {
+    res.status(500).json({
+      message: 'Error on server. Try again later'
+    })
+  }
 })
 
 module.exports = router
